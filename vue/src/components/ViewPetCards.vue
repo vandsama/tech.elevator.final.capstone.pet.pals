9a440c4f25fc9dc1fc239fb4aaf28a60eb768e11
Henry Fossett <henry.fossett@gmail.com>
2022-12-08 14:39:41 -0500

    pet card on homepage

--- vue/src/components/ViewPetCards.vue
+++ vue/src/components/ViewPetCards.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
-    <img v-bind:src="this.pet.img" alt="the goodest boy" />
+  <div class="petCard">
+    <div class="thumbnail">
+      <img v-bind:src="this.pet.img" alt="the goodest boy" />
+    </div>
     <div class="info">
-      {{ this.pet.name }}
-      {{ this.pet.age }}
-      {{ this.pet.type }}
+      <h3>{{ this.pet.name }}</h3>
+      <div class="infoNoName">
+        {{ this.pet.age }} <br />
+        {{ this.pet.type }} <br />
+        Enjoys Swimming
+      </div>
     </div>
   </div>
 </template>
@@ -29,9 +34,47 @@ export default {
     petService.get(2).then((r) => {
       this.pet = r.data;
     });
+    petService.get(3).then((r) => {
+      this.pet = r.data;
+    });
   },
 };
 </script>
 
 <style>
+.petCard {
+  font-family: "Bebas Neue", cursive;
+  font-size: 1.4em;
+  border: 4px solid #5da2d5;
+  background-color: azure;
+  border-radius: 6px;
+  width: max-content;
+}
+.thumbnail {
+  position: relative;
+  width: 150px;
+  height: 150px;
+  overflow: hidden;
+  margin: 10px;
+  border: 5px solid #5da2d5;
+  background-color: #5da2d5;
+  border-radius: 5px;
+}
+.thumbnail img {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  height: 200px;
+  width: auto;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+.thumbnail img.portrait {
+  width: 100%;
+  height: auto;
+}
+.infoNoName {
+  font-family: "Quattrocento Sans", sans-serif;
+}
 </style>
