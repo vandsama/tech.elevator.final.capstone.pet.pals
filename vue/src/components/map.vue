7c50926706fafff7095d12f0273be1421670c079
alexellington1073 <alexellington1073@yahoo.com>
2022-12-13 14:42:56 -0500

    playdate page styling fix

---
+++ vue/src/components/map.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="map">
+      <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly"
+      defer
+    ></script>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
