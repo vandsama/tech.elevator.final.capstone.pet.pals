75a5acee03fdbdeddb43f2a137046fac92f4e8d6
Samantha Vanderburgh <vandsama@gmail.com>
2022-12-13 14:14:44 -0500

    Forum is styled and requirements are filled

--- vue/src/components/TopicDetails.vue
+++ vue/src/components/TopicDetails.vue
@@ -1,30 +1,25 @@
 <template>
-  <div class="topic-details">
+  <div>
     <h1>{{ this.topic.title }}</h1>
-
     <table class="message-table">
       <tbody>
         <tr
           v-for="message in this.messages"
           v-bind:key="message.id"
-          v-bind:message="message"
+          v-bind:message="message" class="message-list-css"
         >
-          <td class="message-list-css">
+          <td>
             <message v-bind:message="message"></message>
           </td>
 
-          <td class="message-list-css">
+          <td>
             {{ message.text }}
           </td>
 
-          <td class="message-list-css">
+          <td>
             {{ message.timestamp | formatDate }}
             {{ message.timestamp | formatTime }}
           </td>
-          <!-- 
-          <td class="message-list-css">
-            {{ message.timestamp | formatTime }}
-          </td> -->
         </tr>
       </tbody>
     </table>
@@ -49,14 +44,16 @@
 
     <!-- </div> -->
 
-    <router-link
-      :to="{
-        name: 'AddMessage',
-        params: { topicId: $store.state.activeTopic.id },
-      }"
-      class="addMessage"
-      >Add New Message</router-link
-    >
+    <div class="create-new-message">
+      <router-link
+        :to="{
+          name: 'AddMessage',
+          params: { topicId: $store.state.activeTopic.id },
+        }"
+        class="addMessage"
+        >Post New Message</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -103,51 +100,65 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .message-table {
   display: flex;
   justify-content: center;
   font-family: "Quattrocento Sans", sans-serif;
+  color: white;
   text-align: left;
+  border-collapse: separate;
+  border-spacing: 0 15px;
 }
 
 .message-list-css {
-  font-family: "Quattrocento Sans", sans-serif;
-  font-size: 1.4em;
-  border: 4px solid #5da2d5;
-  background-color: azure;
-  border-radius: 6px;
-  width: max-content;
+  font-size: large;
+  font-style: italic;
+  background-color: #5da2d5;
+  /* border-radius: 12px; */
+  box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.5);
+  text-align: left;
+  text-decoration: none;
+  padding: 20px;
 }
 
-/** page structure **/
-.topic-details {
-  padding: 20px 20px;
-  margin: 0 auto;
-  max-width: 600px;
+tr, td {
+  padding-right: 25px;
 }
-/** ios1-ios6 bubbles **/
-.addMessage {
-  /* display: block;
-  padding: 0 0 10px 10px; */
-  display: flex;
-  justify-content: center;
-  font-family: "Quattrocento Sans", sans-serif;
-  text-align: left;
-}
-a.addMessage:link,
-a.addMessage:visited {
-  color: #5da2d5;
+a {
+  color: white;
   text-decoration: none;
 }
-a.addMessage:hover {
-  text-decoration: underline;
+a:hover {
+  color: tomato;
 }
-
-.topic-message {
+.create-new-message {
   display: flex;
   justify-content: center;
   font-family: "Quattrocento Sans", sans-serif;
-  text-align: left;
+  background-color: #f5c423;
+  width: 20%;
+  margin: auto;
+  margin-top: 3%;
+  margin-bottom: 2%;
+  border-radius: 15px;
+  box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.5);
 }
+.create-new-message:hover {
+  background-color: #5da2d5;
+  box-shadow: 0px 15px 20px #69dcf0;
+  transform: translateY(-7px);
+  border-radius: 12px;
+  transition: all 0.3s ease 0s;
+}
+input[type="text"] {
+  border-radius: 5px;
+  box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.5);
+  width: 60%;
+}
+input[type="text"]:hover {
+  transform: translateY(-5px);
+  transition: all 0.3s ease 0s;
+}
+
 </style>
